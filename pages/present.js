b0c194553e236c35e4532756321b8e15571c6ab1
Creston Barker <barker.creston@gmail.com>
2022-03-10 17:02:50 -0600

    Eventbright testing

--- pages/present.js
+++ pages/present.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Present.module.scss";
 import Layout from "../components/Layout";
 import { Container, Row, Col } from "react-bootstrap";
+import Script from "next/script";
 
 export default function Present() {
   return (
@@ -11,16 +11,29 @@ export default function Present() {
         <title>Kikapekiskwewin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        src="https://www.eventbrite.ca/static/widgets/eb_widgets.js"
+        onLoad={() => {
+          window.EBWidgets.createWidget({
+            // Required
+            widgetType: "checkout",
+            eventId: "267704028957",
+            iframeContainerId: "eventbrite-widget-container-267704028957",
+            // Optional
+            iframeContainerHeight: 425, // Widget height in pixels. Defaults to a minimum of 425px if not provided
+          });
+        }}
+      ></Script>
       <main className={`${styles["main"]}`}>
-        <Container >
+        <Container>
           <h1>Work In Progress</h1>
           <Row>
             <Col>
-              <p>
-                Check back here for more information coming soon!
-              </p>
+              <p>Check back here for more information coming soon!</p>
             </Col>
           </Row>
+          <div id="eventbrite-widget-container-267704028957" >
+      </div>
         </Container>
       </main>
     </Layout>
