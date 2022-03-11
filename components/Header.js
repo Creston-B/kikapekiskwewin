43cc9456f1c50687f234c1ba15575805a859736f
Creston Barker <barker.creston@gmail.com>
2022-03-10 18:10:52 -0600

    cleaned up styles on header and added icon.

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
-import styles from "../styles/Home.module.scss";
 import Image from "next/dist/client/image";
 
 export default class HeaderComponent extends Component {
@@ -18,40 +17,52 @@ class NavComponent extends Component {
     return (
       <Navbar variant="dark" expand="lg" className={`navbar pt-0`}>
         <Container fluid className="banner">
-          <Row className="container-fluid flex-row">
-            <Col className="container-fluid col-10 col-lg-4 col-2">
+          <Row className="flex-row w-100">
+            <Col className="col-10 col-lg-4">
               <Navbar.Brand href="/" className="nav-brand">
-                <div className="kikapekiskwewin-logo-text">
-                  <Image
-                    src="/kikapekiskwewin-logo-crop-stroke.png"
-                    objectFit="contain"
-                    layout="fill"
-                    alt="kikapekiskwewin logo"
-                  />
-                </div>
+                <Container fluid className="flex-column flex-nowrap">
+                  <Row className="kikapekiskwewin-logo pt-3">
+                    <Col className="kikapekiskwewin-logo-icon col-8 col-sm-3">
+                      <Image
+                        src="/kikapekiskwewin-logo-2-icon.png"
+                        objectFit="contain"
+                        layout="fill"
+                        alt="kikapekiskwewin logo icon"
+                      />
+                    </Col>
+                    <Col className="kikapekiskwewin-logo-text col-8">
+                      <Image
+                        src="/kikapekiskwewin-logo-crop-stroke.png"
+                        objectFit="contain"
+                        layout="fill"
+                        alt="kikapekiskwewin logo"
+                      />
+                    </Col>
+                  </Row>
+                </Container>
               </Navbar.Brand>
             </Col>
-            <Col className="col-lg-8 col-1 align-self-center">
+            <Col className="col-1 col-lg-4 align-self-center">
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
             </Col>
           </Row>
           <Row className="container-fluid">
-              <Navbar.Collapse id="basic-navbar-nav justify-content-end">
-                <Nav justify className="ml-auto text-end">
-                  <Nav.Link className="nav-item" href="/location">
-                    <strong>Location</strong>
-                  </Nav.Link>
-                  <Nav.Link className="nav-item" href="/people">
-                    <strong>Team &amp; Presenters</strong>
-                  </Nav.Link>
-                  <Nav.Link className="nav-item" href="/schedule">
-                    <strong>Schedule</strong>
-                  </Nav.Link>
-                  <Nav.Link className="nav-item" href="/present">
-                    <strong>Present</strong>
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
+            <Navbar.Collapse id="basic-navbar-nav justify-content-end">
+              <Nav justify className="ml-auto text-end">
+                <Nav.Link className="nav-item" href="/location">
+                  <strong>Location</strong>
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="/people">
+                  <strong>Team &amp; Presenters</strong>
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="/schedule">
+                  <strong>Schedule</strong>
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="/present">
+                  <strong>Present</strong>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Row>
         </Container>
       </Navbar>
