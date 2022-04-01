e86ab2686ce27c3777d62faac3e5b083f1e44036
Creston Barker <barker.creston@gmail.com>
2022-04-01 10:03:17 -0500

    modified sitemap file to generate dynamically

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,3 +1,5 @@
+import * as fs from 'fs'
+
 const Sitemap = () => {
   return null;
 };
@@ -5,36 +7,36 @@ const Sitemap = () => {
 export const getServerSideProps = async ({ res }) => {
   const BASE_URL = "http://kikpekiskwewin.ca";
 
-  const sitemap = `
-  <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
-<url>
-<loc>https://www.kikapekiskwewin.ca/</loc>
-<lastmod>2022-04-01T14:47:32+00:00</lastmod>
-<priority>1.00</priority>
-</url>
-<url>
-<loc>https://www.kikapekiskwewin.ca/location</loc>
-<lastmod>2022-04-01T14:47:32+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc>https://www.kikapekiskwewin.ca/people</loc>
-<lastmod>2022-04-01T14:47:32+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc>https://www.kikapekiskwewin.ca/schedule</loc>
-<lastmod>2022-04-01T14:47:32+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc>https://www.kikapekiskwewin.ca/register</loc>
-<lastmod>2022-04-01T14:47:32+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-</urlset>
+  const staticPaths = fs
+      .readdirSync("pages")
+      .filter((staticPage) => {
+        return ![
+          "api",
+          "_app.js",
+          "_document.js",
+          "404.js",
+          "sitemap.xml.js",
+        ].includes(staticPage);
+      })
+      .map((staticPagePath) => {
+        return `${BASE_URL}/${staticPagePath}`;
+      });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPaths
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
 `;
 
   res.setHeader("Content-Type", "text/xml");
