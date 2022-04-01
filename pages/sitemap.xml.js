1f62a338af70d9a2b764bc3ee9f116c34e3d9ea6
Creston Barker <barker.creston@gmail.com>
2022-04-01 10:11:25 -0500

    modified sitemap to work on vercel production

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,4 @@
-import * as fs from 'fs'
+import * as fs from "fs";
 
 const Sitemap = () => {
   return null;
@@ -8,19 +8,24 @@ export const getServerSideProps = async ({ res }) => {
   const BASE_URL = "https://www.kikpekiskwewin.ca";
 
   const staticPaths = fs
-      .readdirSync("pages")
-      .filter((staticPage) => {
-        return ![
-          "api",
-          "_app.js",
-          "_document.js",
-          "404.js",
-          "sitemap.xml.js",
-        ].includes(staticPage);
-      })
-      .map((staticPagePath) => {
-        return `${BASE_URL}/${staticPagePath}`;
-      });
+    .readdirSync(
+      {
+        development: "pages",
+        production: "./",
+      }[process.env.NODE_ENV]
+    )
+    .filter((staticPage) => {
+      return ![
+        "api",
+        "_app.js",
+        "_document.js",
+        "404.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath}`;
+    });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
