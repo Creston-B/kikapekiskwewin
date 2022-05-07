20bf2277e48e7b067b07412b272080f335e73881
Creston <75334273+Creston-B@users.noreply.github.com>
2022-05-06 22:01:50 -0500

    Update Footer.js
    typo fix

--- components/Footer.js
+++ components/Footer.js
@@ -39,7 +39,7 @@ export default class FooterComponent extends Component {
           </Col>
           <Col className="m-2">
             © Creston Barker -{" "}
-            <a href="mailto:creston@barkerdev.com">creston@barkedev.com</a> 2021
+            <a href="mailto:creston@barkerdev.com">creston@barkerdev.com</a> 2021
           </Col>
           <div className="w-100 d-block d-sm-none"></div>
           <Col className="m-2">
